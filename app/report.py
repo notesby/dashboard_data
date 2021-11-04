f919e587a637fc0fbe5460f6e20aa48befd33cff
hector <dhmoreno92@gmail.com>
2021-11-03 21:54:46 -0600

    added donwload pdf

--- app/report.py
+++ app/report.py
@@ -1,10 +1,12 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for, session
+    Blueprint, flash, g, redirect, render_template, request, url_for, session, send_from_directory
 )
 
 from app.auth import login_required
-from app.models import db, Report
-
+from app.models import db, Report, SavedGraph
+from app.graph import load_geojson, run_query, generate_graph, display_graph
+import pdfkit
+import os
 
 bp = Blueprint('reports', __name__)
 
@@ -27,5 +29,25 @@ def create():
         return redirect(url_for("reports.index"))
 
     return render_template('addreport.html')
+
+
+@bp.route('/report/<int:report_id>/download')
+def download(report_id):
+    graphs = SavedGraph.query.filter_by(report_id=report_id)
+    graphs = list([x for x in graphs])
+    for i, graph in enumerate(graphs):
+        data = run_query(graph, graph.fields)
+        geojson = None
+        if graph.geojson:
+            geojson = load_geojson(graph.geojson)
+        fig = generate_graph(data, geojson, graph.graph_options, graph.type)
+        graphs[i].image = display_graph(fig, "image")
+
+    html = render_template('pdftemplate.html', graphs=graphs)
+    root = os.path.realpath(os.path.dirname(__file__))
+    pdf_path = os.path.join(root, "static", f"{report_id}.pdf")
+    pdfkit.from_string(html, pdf_path)
+    
+    return send_from_directory(os.path.join(root, "static"), f"{report_id}.pdf")
 
 
